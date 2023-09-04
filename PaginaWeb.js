22a828f719c03367c3841bad212bc07af5a38a38
Francisco Villalba <villalbaportillaf.fv@gmail.com>
2023-09-04 18:56:47 -0500

    Informacion sobre compiladores acabada

---
+++ PaginaWeb.js
@@ -0,0 +1,19 @@
+document.getElementById('Compiladores').addEventListener('click', function() {
+    const target = this.getAttribute('data-target');
+    if (target) {
+        const destino = document.querySelector(target);
+        if (destino) {
+            destino.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+});
+
+document.getElementById('About').addEventListener('click', function() {
+    const target = this.getAttribute('data-target');
+    if (target) {
+        const destino = document.querySelector(target);
+        if (destino) {
+            destino.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+});

32a84028733eec8efa0cdd9f8db86acd38e5e903
Francisco Villalba <villalbaportillaf.fv@gmail.com>
2023-09-06 21:17:55 -0500

    Finalizado

--- PaginaWeb.js
+++ PaginaWeb.js
@@ -17,3 +17,12 @@ document.getElementById('About').addEventListener('click', function() {
         }
     }
 });
+document.getElementById('Lenguajes').addEventListener('click', function() {
+    const target = this.getAttribute('data-target');
+    if (target) {
+        const destino = document.querySelector(target);
+        if (destino) {
+            destino.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+});
